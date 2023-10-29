fd96e52dfd895ca3fb78b5234dbe8c19e86533a5
Stephan <5377412+Taschi120@users.noreply.github.com>
2023-10-29 14:14:56 +0100

    UI wip

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+class_name SettingsWindow
+
+signal quit_button_pressed
+signal about_button_pressed
+signal settings_button_pressed
+signal return_to_game_pressed
+signal new_game_button_pressed
+
+
+func _on_return_to_game_pressed() -> void:
+	return_to_game_pressed.emit()
+
+func _on_start_button_pressed() -> void:
+	new_game_button_pressed.emit()
+
+func _on_settings_button_pressed() -> void:
+	settings_button_pressed.emit()
+
+func _on_about_button_pressed() -> void:
+	about_button_pressed.emit()
+	
+func _on_quit_button_pressed() -> void:
+	quit_button_pressed.emit()
